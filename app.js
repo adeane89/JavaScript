08eb3c1f2921356e778ffbffc3794d6fa59b1f79
alexis <37817243+adeane89@users.noreply.github.com>
2018-07-09 14:57:49 -0500

    2 probs done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 (function() {
     console.log("Hello world");
 }());
@@ -5,3 +6,28 @@
 function add(n1, n2){
     return n1+n2;
 }
+*/
+
+//problem one
+function greater(n1, n2){
+    if (n1 > n2) {
+        console.log(n1);
+    } else {
+        console.log(n2);
+    };
+};
+
+//problem two
+function sort(n1, n2, n3) {
+    //if ((n1 > n2) && (n1 > n3)
+    var greatest = n1;
+    if(n2 > greatest)
+            {
+                greatest = n2;
+            }
+            if(n3 > greatest)
+            {
+                greatest = n3;
+            };
+    alert(greatest);
+};
