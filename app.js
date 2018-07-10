366c8578662878143b0a7dff027f3535d8dd16fd
alexis <37817243+adeane89@users.noreply.github.com>
2018-07-10 09:37:27 -0500

    Problems all done, 8 half there

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-/*
-(function() {
-    console.log("Hello world");
-}());
-
-function add(n1, n2){
-    return n1+n2;
-}
-*/
-
 //problem one
-//solved
 function greater(n1, n2){
     if (n1 > n2) {
         console.log(n1);
@@ -18,37 +7,34 @@ function greater(n1, n2){
     }
 }
 
-//greater();
 //problem two
-//not solved
 function sort(num1, num2, num3) {
-            if((num1> num2) && (num2 > num3))
+            if(num1 > num2 && num2 > num3)
             {
-                alert(num1, num2, num3);
+                alert(num1 + ", " + num2 + ", " + num3);
             }
-            else if ((num2 > num1) && (num1 > num3))
+            else if (num2 > num1 && num1 > num3)
             {
-                alert(num2, num1, num3);
+                alert(num2 + ", " + num1 + ", "+ num3);
             }
-            else if  ((num2 > num3) && (num3 > num1))
+            else if  (num2 > num3 && num3 > num1)
             {
-                alert(num2, num3, num1);
+                alert(num2 + ", "+ num3 + ", "+  num1);
             }
-            else if  ((num3 > num1) && (num1 > num2))
+            else if  (num3 > num1 && num1 > num2)
             {
-                alert(num3, num1, num2);
+                alert(num3 + ", "  +num1 + ", " + num2);
             }
-            else if  ((num3 > num2) && (num2 > num1))
+            else if  (num3 > num2 && num2 > num1)
             {
-                alert(num3, num2, num1);
+                alert(num3 + ", "+ num2 + ", " +num1);
             }
             else {
-                alert(num1, num3, num2);
+                alert(num1 + ", " +num3 + ", "+  num2);
             }
 }
-//sort();
+
 //problem three
-//solved
 function greatest(n1, n2, n3, n4, n5) {
     var greatest = n1;
     if(n2 > greatest)
@@ -69,10 +55,8 @@ function greatest(n1, n2, n3, n4, n5) {
             }
     alert(greatest);
 }
-//greatest();
 
 //problem four
-//solved
 function oddEvenLoop(){
   for (var i = 0; i <= 15; i++) {
       if (i%2 === 0) {
@@ -83,10 +67,7 @@ function oddEvenLoop(){
   }
 }
 
-//oddEvenLoop();
-
 //problem five
-//solved
 function fizzBuzz(){
             for (var i = 1; i <= 100; i++) {
                 if (i % 15 === 0) {
@@ -104,7 +85,6 @@ function fizzBuzz(){
 }
 
 //problem six
-//solved
 function thousand(){
     var count = 0;
     for (var i = 1; i <= 1000; i++){
@@ -116,34 +96,70 @@ function thousand(){
 }
 
 //problem seven
-/*
-function triangle(){
-    for (var i = 1; i <= 5; i++) {
-        var num = "";
-        for (var j = 1; j <= 5-i; j++) {
-            num += " ";
-        } for (var k = 1; k <= i; k++) {
-            num += j;
+function triangle() {
+    for(var i=1; i < 6; i++) {
+        var output="";
+        for(var j=1; j < 6-i; j++) {
+            output+=" ";
         }
-        console.log(num);
+        for(var k=1; k <= i; k++) {
+           output += k;
+        }
+        for (var m=i-1; m >= 1; m--) {
+            output += m;
+        }
+        console.log(output);
     }
 }
-*/
 
-function generatePyramid() {
-  let number = '';
-
-  for (let i = 1; i <= 5; i++) {
-    console.log(number += i +(number));
-  }
+//problem eight
+function mirror(){
+    for(var i=1; i < 6; i++) {
+        var output = "";
+        for(var j=1; j < 6-i; j++) {
+            output +=" ";
+        }
+        for(var k=1; k <= i; k++) {
+           output += k;
+        }
+        for (var m=i-1; m >= 1; m--) {
+            output += m;
+        }
+        console.log(output);
+    }
+        for(var i=4; i >=1; i--) {
+            var output ="";
+            for(var j=3; j > i; j--) {
+                output += " ";
+            }
+            for(var k=4; k >= i; k--) {
+                output += k;
+            }
+            for (var m=i+1; m <= 4; m++) {
+                output += m;
+            }
+             console.log(output);
+    }
 }
 
 
+//didnt work
+/*
+        if (i === 6) {
+        for(var r=6; r > 6; r--) {
 
+            output="";
+        for(var s=6; s < 6+r; s++) {
+            output+=" ";
+        }
+        for(var t=r-1; t >= r; t--) {
+           output += t;
+        }
+        for (var y=6; y >= 1; y--) {
+            output += y;
+        }
+        }
+        }
 
-
-
-
-
-
+        */
 
